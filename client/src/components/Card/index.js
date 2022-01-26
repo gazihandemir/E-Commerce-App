27be3faafb2b68651e6010c87b3a455c34f350fd
gazihandemir <gazihand@gmail.com>
2022-01-26 10:52:12 +0300

    react query ile datalar database'den çekildi

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Box, Image, Button } from "@chakra-ui/react";
+import moment from "moment";
 import { Link } from "react-router-dom";
-function Card() {
+function Card({ item }) {
   return (
     <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="3">
       <Link to="/">
-        <Image src="https://picsum.photos/200/300" alt="product" />
+        <Image src={item.photos[0]} alt="product" loading="lazy"/>
         <Box p="6">
           <Box d="flex" alignItems="baseline">
-            12/12/2021
+            {moment(item.createdAt).format("DD/MM/YYYY")}
           </Box>
           <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-            Macbook Pro
+            {item.title}
           </Box>
-          <Box>100TL</Box>
+          <Box>{item.price}</Box>
         </Box>
       </Link>
       <Button colorScheme="pink">Add to basket</Button>
