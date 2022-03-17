49f528bd88b1d9fdd1e3c2fd70f74d20fbba676d
gazihandemir <gazihand@gmail.com>
2022-03-17 19:14:22 +0300

    signup tasarım ,formik ve validation yapıldı

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,9 +1,76 @@
-import React from 'react';
-
+import React from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import validationSchema from "./validations";
 function Signup() {
-  return <div>
-      sign up
-  </div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    onSubmit: async (values, bag) => {
+      console.log(values);
+    },
+    validationSchema,
+  });
+
+  return (
+    <div>
+      <Flex align={"center"} width={"full"} justifyContent={"center"}>
+        <Box pt={10}>
+          <Box textAlign={"center"}>
+            <Heading>Sign Up</Heading>
+          </Box>
+          <Box my={"5px"} textAlign={"left"}>
+            <form onSubmit={formik.handleSubmit} autoComplete="off">
+              <FormControl>
+                <FormLabel>E-mail</FormLabel>
+                <Input
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+              </FormControl>
+
+              <FormControl mt={"4"}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="password"
+                  type={"password"}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                />
+              </FormControl>
+              <FormControl mt={"4"}>
+                <FormLabel>Password Confirm</FormLabel>
+                <Input
+                  name="passwordConfirm"
+                  type={"password"}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.passwordConfirm}
+                />
+              </FormControl>
+              <Button mt={"4"} width={"full"} type={"submit"}>
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
 export default Signup;
---
+++ client/src/pages/Auth/Signup/validation.js
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const validations = yup.object().shape({
+  email: yup
+    .string()
+    .email("Geçerli bir email giriniz.")
+    .required("Zorunlu alan"),
+  password: yup
+    .string()
+    .min(5, "Parolanız en az 5 karakter olmalıdır.")
+    .required(),
+  passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref("password")], "Parolalar uyuşmuyor"),
+});
+
+export default validations;
